13c5761ee89ced212f9ee134145a7ea4282ab158
mtw-programmer <visnia58@gmail.com>
2023-06-06 22:38:10 +0200

    feat: add NODE_ENV variable to package.json on dev, import config in index.ts, server works on PORT config variable

--- api/index.ts
+++ api/index.ts
@@ -1,5 +1,8 @@
 import express from 'express';
+import dotenv from 'dotenv';
+dotenv.config({ path: `config/${process.env.NODE_ENV}.env` });
 
 const app = express();
 
-app.listen(3000, () => console.log('App listening on port 3000...'))
+const port = process.env.PORT || 3001;
+app.listen(port, () => console.log(`App listening on port ${port}...`))
