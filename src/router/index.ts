a6cdeaf827370bebdd14e4b8962f71a04b1f3ec2
ReQ1964 <slomianykacper4@gmail.com>
2023-06-07 15:54:55 +0200

    feat: add HomeView page to router

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,13 @@
 import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router';
+import HomeView from '../views/HomeView.vue';
 
-const routes: Array<RouteRecordRaw> = [];
+const routes: Array<RouteRecordRaw> = [
+	{
+		path: '/',
+		name: 'home',
+		component: HomeView,
+	},
+];
 
 const router = createRouter({
 	history: createWebHashHistory(),
