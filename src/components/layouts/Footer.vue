78cdf267f47213fb50fe79b76bd071c9733fe64a
ReQ1964 <94235105+ReQ1964@users.noreply.github.com>
2023-06-07 20:11:09 +0200

    Add Footer component (#10)
    * feat: add call, chat and stories icons for footer
    
    * style: add secondary bg color to variables
    
    * feat: add Footer layout component with router-links
    
    * feat: monitor footer links' active state
    
    * feat: add Footer to App template
    
    * fix: repair variables pull merge bug

---
+++ src/components/layouts/Footer.vue
@@ -0,0 +1,85 @@
+<script lang="ts">
+
+export default {
+  name: 'Footer',
+  data() {
+    return {
+      currentPath: this.$route.path
+    }
+  },
+  watch: {
+    $route(to) {
+      this.currentPath = to.path;
+    }
+  }
+}
+
+</script>
+<template>
+  <footer>
+    <router-link :to="{ path: '/' }" exact>
+      <div :class="currentPath === '/' ? [$style.item, $style.active] : $style.item">
+        <img src="../../assets/icons/footer/chat.svg" alt="">
+        <p>Chats</p>
+      </div>
+    </router-link>
+    <router-link :to="{ path: '/calls' }" exact>
+      <div :class="currentPath === '/calls' ? [$style.item, $style.active] : $style.item">
+        <img src="../../assets/icons/footer/call.svg" alt="">
+        <p>Calls</p>
+      </div>
+    </router-link>
+    <router-link :to="{ path: '/stories' }" exact>
+      <div :class="currentPath === '/stories' ? [$style.item, $style.active] : $style.item">
+        <img src="../../assets/icons/footer/stories.svg" alt="">
+        <p>Stories</p>
+      </div>
+    </router-link>
+  </footer>
+</template>
+
+
+
+<style module lang="scss">
+@use '../../main.scss' as *;
+
+footer {
+  position: fixed;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  background-color: $bg-color-secondary;
+  width: 100%;
+  bottom: 0;
+
+  a {
+    text-decoration: none;
+    color: $txt-color-primary;
+  }
+
+  .item {
+    @include flex-center;
+    flex-direction: column;
+    gap: 0.3rem;
+    padding: 0.7rem;
+
+    img {
+      padding: 0.5rem;
+      width: 45px;
+    }
+
+    p {
+      font-size: .9rem;
+    }
+
+  }
+
+  .active {
+    img {
+      background-color: #cad2c556;
+      border-radius: 0.5rem;
+    }
+  }
+
+}
+</style>
