4afe09e5c2069ce5749618ffe934cb74b8fd536d
ReQ1964 <slomianykacper4@gmail.com>
2023-06-07 13:50:15 +0200

    feat: add basic router structure with navigation component

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,17 @@
-<script setup lang="ts">
+<script lang="ts">
+import Navigation from './components/layouts/Navigation.vue';
+import './main.scss'
 
+export default {
+  components: {
+    Navigation
+  }
+}
 </script>
 
-<template></template>
+<template>
+  <Navigation />
+  <router-view />
+</template>
 
-<style scoped></style>
+<style lang="scss"></style>
