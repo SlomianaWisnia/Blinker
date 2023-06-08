ac81e3cae1241cc53151b701140164aebff3c5d2
mtw-programmer <visnia58@gmail.com>
2023-06-08 12:14:36 +0200

    style: remove comments from jest config file

--- api/jest.config.ts
+++ api/jest.config.ts
@@ -6,15 +6,5 @@ export default {
   collectCoverage: true,
   coverageDirectory: "coverage",
   coverageProvider: "v8",
-  // An array of file extensions your modules use
-  // moduleFileExtensions: [
-  //   "js",
-  //   "jsx",
-  //   "ts",
-  //   "tsx",
-  //   "json",
-  //   "node"
-  // ],
-
   verbose: true,
 };
