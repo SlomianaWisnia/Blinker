dccd18161c570c9a24b70eca2bdc20ab3d89696b
ReQ1964 <slomianykacper4@gmail.com>
2023-06-21 18:26:00 +0200

    feat: add axios default config and import

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,11 @@
 import { createApp } from 'vue';
 import { plugin, defaultConfig } from '@formkit/vue';
+import axios from 'axios';
 import './assets/scss/reset.scss';
 import App from './App.vue';
 import router from './router';
+
+axios.defaults.baseURL = 'http://localhost:3002/api';
+axios.defaults.withCredentials = true;
 
 createApp(App).use(plugin, defaultConfig).use(router).mount('#app');
