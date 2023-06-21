c6d98169ddd78c87d67a4b63d9c13e2c0ca03713
mtw-programmer <visnia58@gmail.com>
2023-06-22 00:43:56 +0200

    feat: add data properties to auth-verify route response
    now contains user username, email and avatar if is set and same for friends

--- api/controllers/auth-verify.ts
+++ api/controllers/auth-verify.ts
@@ -1,10 +1,22 @@
+import RequestSession from '../interfaces/RequestSession';
 import Router, { Response } from 'express';
+import User from '../models/User';
+import _ from 'lodash';
 import log from '../utils/log';
 const router = Router();
 
-router.post('/', async (_req, res:Response) => {
+router.post('/', async (req:RequestSession, res:Response) => {
   try {
-    return res.json({ msg: 'Session verification sucessful!' });
+    const { userId } = req.session;
+    const user = await User.findOne({ _id: userId }).lean().select('-_id username email avatar friends');
+    const friends = await User.find({ _id: user.friends }).select('-_id username avatar');
+
+    delete user.friends;
+
+    return res.json({
+      msg: 'Session verification sucessful!',
+      data: { user, friends }
+    });
   } catch (ex) {
     log.error({ label: 'Auth Verify', message: ex });
     return res.status(500).json({ msg: 'Something went wrong! Please, try again later.' });
