f62038e4f62b530edc15797d2f2e1f57bbf5cc1c
ReQ1964 <slomianykacper4@gmail.com>
2023-06-22 17:56:50 +0200

    feat: vuex add user_info state and addUserInfo mutation

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,5 +1,17 @@
 import { createStore } from 'vuex';
+import UserInfo from '../interfaces/UserInfo';
 
-const store = createStore({});
+const store = createStore({
+	state() {
+		return {
+			user_info: [],
+		};
+	},
+	mutations: {
+		addUserInfo(state: any, payload: UserInfo) {
+			state.user_info = payload;
+		},
+	},
+});
 
 export default store;
