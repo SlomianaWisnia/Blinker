e87bd8c78c732453c11512075470f904d8bc5785
mtw-programmer <visnia58@gmail.com>
2023-07-03 23:56:30 +0200

    refactor: change ChatRoom _id to proper ts type

--- api/interfaces/models/ChatRoom.ts
+++ api/interfaces/models/ChatRoom.ts
@@ -1,9 +1,9 @@
-import { Document } from 'mongoose';
+import mongoose, { Document } from 'mongoose';
 import User from './User';
 import Message from './Message';
 
 export default interface ChatRoom extends Document {
-  _id: string;
+  _id: mongoose.Schema.Types.ObjectId;
   members: User['_id'][];
   messages: Message[];
 };
