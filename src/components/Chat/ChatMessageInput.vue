f47a908db3e4374510fb8ba45f7422c235ae0a58
ReQ1964 <slomianykacper4@gmail.com>
2023-07-11 20:09:07 +0200

    feat: add ChatMessageInput with dummy message send func

---
+++ src/components/Chat/ChatMessageInput.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="chatMessageInput">
+    <div class="chatBar">
+      <img src="../../assets/icons/chats/emoji_menu.svg" alt="">
+      <FormKit type="form" @submit="" :actions="false" :errors="[]">
+        <FormKit type="textarea" placeholder="Type a message..." wrapper-class="chatBox" rows="1" resize />
+      </FormKit>
+      <img src="../../assets/icons/chats/camera.svg" alt="">
+      <img src="../../assets/icons/chats/voice_message.svg" alt="">
+    </div>
+  </div>
+</template>
+<script lang="ts">
+export default {
+  name: 'chatMessageInput',
+  computed: {
+    chatroomId() {
+      return this.$route.params.chatId
+    }
+  },
+  methods: {
+    sendMessage() {
+      console.log(this.chatroomId)
+    }
+  }
+}
+</script>
+<style lang="scss">
+.chatMessageInput {
+  background-color: $bg-color-primary;
+  padding: 0.7rem 1rem 0.7rem 1rem;
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+
+
+  .chatBar {
+    display: flex;
+    width: 100%;
+    gap: 0.5rem;
+    background-color: $bg-color-secondary;
+    border-radius: 1.25rem;
+    padding: 0.1rem 0.7rem;
+
+    img {
+      width: 33px;
+    }
+  }
+
+  .chatBox {
+
+    .formkit-inner {
+      .formkit-input {
+        background-color: $bg-color-secondary;
+        border: none;
+        padding: 0.7rem;
+        color: $txt-color-primary;
+        font-size: 1rem;
+        width: 95%;
+        resize: none;
+      }
+    }
+
+  }
+}
+</style>
--- src/layouts/ChatLayout.vue
+++ src/layouts/ChatLayout.vue
@@ -1,15 +1,18 @@
 <template>
   <ChatNavbar />
   <router-view />
+  <ChatMessageInput />
 </template>
 
 <script lang="ts">
 import ChatNavbar from '../components/Chat/ChatNavbar.vue';
+import ChatMessageInput from '../components/Chat/ChatMessageInput.vue';
 
 export default {
   name: "ChatLayout",
   components: {
     ChatNavbar,
+    ChatMessageInput
   }
 };
 </script>
