86b1555ad0b07fb08ee00a5454dede0fb1110bb9
ReQ1964 <slomianykacper4@gmail.com>
2023-07-23 15:22:55 +0200

    refactor: migrate AuthView to composition api, change functions' names

--- src/views/AuthView.vue
+++ src/views/AuthView.vue
@@ -1,29 +1,20 @@
 <template>
-  <LoginForm :authMethodHandler="checkAuthMethod" v-if="authMethod === 'login'" />
-  <SignUpForm :authMethodHandler="checkAuthMethod" v-if="authMethod === 'register'" />
+  <LoginForm :switch-auth-method="switchAuthMethod" v-if="authMethod === 'login'" />
+  <SignUpForm :switch-auth-method="switchAuthMethod" v-if="authMethod === 'register'" />
 </template>
-<script lang="ts">
+
+<script setup lang="ts">
+import { ref } from 'vue';
 import LoginForm from '../components/Auth/LoginForm.vue'
 import SignUpForm from '../components/Auth/SignUpForm.vue';
 
-export default {
-  name: 'AuthView',
-  components: {
-    LoginForm,
-    SignUpForm
-  },
-  data() {
-    return {
-      authMethod: 'login' as string
-    }
-  },
-  methods: {
-    checkAuthMethod(method: string) {
-      this.authMethod = method;
-    }
-  }
+const authMethod = ref('login')
+
+const switchAuthMethod = (method: string) => {
+  authMethod.value = method
 }
 </script>
+
 <style lang="scss">
 .auth {
   display: flex;
