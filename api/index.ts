b99cca5201b26a5739ad6d6d4871604d6bebda0b
mtw-programmer <visnia58@gmail.com>
2023-07-24 19:46:54 +0200

    feat: create the very basic version of socket.io rooms
    create middleware for socket auth, use helmet, move express middleware, sessions are stored in mongodb, user joins to all friends chat rooms

--- api/index.ts
+++ api/index.ts
@@ -1,13 +1,18 @@
 import express from 'express';
+import { Server as SocketIOServer, Socket } from 'socket.io';
+import http from 'http';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import session from 'express-session';
+import MongoDBStore from 'connect-mongo';
+import authorizationSocket from './middleware/socket/auth';
 import cookieParser from 'cookie-parser';
+import helmet from 'helmet';
 import './services/db';
 import swaggerUI from 'swagger-ui-express';
 import specs from './docs/index';
 import auth from './routes/auth';
-import authorization from './middleware/auth';
+import authorization from './middleware/express/auth';
 import register from './routes/register';
 import getLastMessages from './routes/getLastMessages';
 import getChatMessages from './routes/getChatMessages';
@@ -16,9 +21,16 @@ import authVerify from './routes/auth-verify';
 import logOut from './routes/logOut';
 import log from './utils/log';
 
+import ChatRoom from './models/ChatRoom';
+
 dotenv.config({ path: `config/${process.env.NODE_ENV}.env` });
 
 const app = express();
+const httpServer = http.createServer(app);
+const io = new SocketIOServer(httpServer, {
+  cors: { origin: process.env.REQUEST_DOMAIN, credentials: true }
+});
+
 app.use(express.json());
 
 const corsOptions = {
@@ -26,21 +38,43 @@ const corsOptions = {
   credentials: true,
   optionSuccessStatus: 200
 };
+
+const store = MongoDBStore.create({
+  mongoUrl: `mongodb://${process.env.DB_HOST}/${process.env.DB_NAME}`,
+  collectionName: 'sessions',
+  autoRemove: 'interval',
+  stringify: false,
+  ttl: 3 * 60 * 60 //3H
+});
+
+
+const sessionMiddleware = session({
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: false,
+  store,
+  cookie: {
+      maxAge: 3 * 60 * 60 * 1000, //3H
+      httpOnly: true,
+  },
+});
+
 app.use(cors(corsOptions));
 
 app.use(cookieParser());
 
-app.use(
-  session({
-      secret: process.env.SESSION_SECRET,
-      resave: true,
-      saveUninitialized: false,
-      cookie: {
-          maxAge: 3 * 60 * 60 * 1000, //3H
-          httpOnly: true,
-      },
-  })
-);
+app.use(sessionMiddleware);
+
+app.use(helmet());
+
+io.use(function (socket:Socket & { request: { res: object } }, next: () => void) {
+  sessionMiddleware(socket.request, socket.request.res, next);
+});
+
+io.use(authorizationSocket);
+
+io.engine.use(helmet());
+
 if (process.env.NODE_ENV === 'development') {
   app.use('/docs', swaggerUI.serve, swaggerUI.setup(specs));
 }
@@ -52,7 +86,14 @@ app.use('/api/send-message', [authorization, sendMessage]);
 app.use('/api/auth-verify', [authorization, authVerify]);
 app.use('/api/logout', logOut);
 
-const port = process.env.PORT || 3001;
-const server = app.listen(port, () => log.init({ label: 'APP', message: `App listening on port ${port}` }));
+io.on('connection', async (socket:any) => {
+  const chatrooms = await ChatRoom.find({ members: socket.request.session.userId }).select('_id');
+  chatrooms.forEach(c => {
+    socket.join(c._id);
+  });
+});
 
-export default server;
+const port = process.env.PORT || 3001;
+
+const server = httpServer.listen(port, () => log.init({ label: 'APP', message: `App listening on port ${port}` }));
+export { store, server, io };
