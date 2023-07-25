1be177ef79232895f486b6c51795395dc8a7f3a8
ReQ1964 <slomianykacper4@gmail.com>
2023-07-25 18:15:42 +0200

    refactor: derive SettingsLayout logic to SettingsNavbar

--- src/layouts/SettingsLayout.vue
+++ src/layouts/SettingsLayout.vue
@@ -1,31 +1,10 @@
 <template>
-  <nav :class="$style.settingsNavbar">
-    <router-link to="/"><img src="../assets/icons/navigation/arrow_back.svg" alt="" /></router-link>
-    <h2>{{ currentRouteName }}</h2>
-  </nav>
+  <SettingsNavbar />
   <router-view />
 </template>
 
 <script setup lang="ts">
-import { computed } from 'vue';
-import { useRoute } from 'vue-router';
-
-const route = useRoute()
-
-const currentRouteName = computed(() => {
-  let routeName = route.name as string;
-  return routeName ? routeName.charAt(0).toUpperCase() + routeName.slice(1) : '';
-});
-
+import SettingsNavbar from '@/components/settings/SettingsNavbar.vue'
 </script>
 
-<style module lang="scss">
-.settingsNavbar {
-  justify-content: flex-start;
-  gap: 1rem;
-
-  img {
-    width: 35px;
-  }
-}
-</style>
+<style module lang="scss"></style>
