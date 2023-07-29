53f4fb18d1da1e098825251b3ddf87a874c7b8e1
mtw-programmer <visnia58@gmail.com>
2023-07-29 18:05:26 +0200

    refactor: add ChatRoom and Message interfaces to arguments

--- api/controllers/getLastMessages.ts
+++ api/controllers/getLastMessages.ts
@@ -2,15 +2,17 @@ import RequestSession from '../interfaces/RequestSession';
 import Router, { Response } from 'express';
 import log from '../utils/log';
 import ChatRoom from '../models/ChatRoom';
-import { encrypt, decrypt } from '../services/encrypt';
+import ChatRoomInterface from '../interfaces/models/ChatRoom';
+import Message from '../interfaces/models/Message';
+import { decrypt } from '../services/encrypt';
 const router = Router();
 
 router.get('/', async (req:RequestSession, res:Response) => {
   try {
     const { userId } = req.session;
     const result = await ChatRoom.find({ members: userId }).select('members messages').slice('messages', -1).populate('members messages.from', '-_id username avatar avatarHex');
-    const decryptedResult = result.map((room) => {
-    const decryptedMessages = room.messages.map((message) => {
+    const decryptedResult = result.map((room:ChatRoomInterface) => {
+    const decryptedMessages = room.messages.map((message:Message) => {
       const decryptedContent = message.message ? decrypt(message.message) : '';
         return decryptedContent ? {
           ...message.toObject(),
