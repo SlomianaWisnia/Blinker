dfa595fa013e4105da6d26815dc9014a8bbb962e
ReQ1964 <slomianykacper4@gmail.com>
2023-08-30 18:04:20 +0200

    feat: add Profile route with dummy content

---
+++ src/views/ProfileView.vue
@@ -0,0 +1,11 @@
+<template>
+  <main>
+    <ProfilePhoto />
+  </main>
+</template>
+
+<script setup lang="ts">
+import ProfilePhoto from '@/components/profile/ProfilePhoto.vue';
+</script>
+
+<style lang="scss"></style>
