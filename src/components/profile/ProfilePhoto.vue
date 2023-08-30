07cd07844f8b0203c9024c24f9f7343ce569b31b
ReQ1964 <slomianykacper4@gmail.com>
2023-08-30 19:06:02 +0200

    feat: add ProfilePhoto, add going back one route in RouteDefaultNavbar

--- src/components/profile/ProfilePhoto.vue
+++ src/components/profile/ProfilePhoto.vue
@@ -1,8 +1,35 @@
 <template>
-  <div></div>
+  <div :class="$style.profilePhoto">
+    <UserAvatar :size="110" />
+    <button>Edit photo</button>
+  </div>
 </template>
 
 <script setup lang="ts">
+import UserAvatar from '@/components/reusable/UserAvatar.vue';
 </script>
 
-<style lang="scss"></style>
+<style module lang="scss">
+.profilePhoto {
+  @include flex-center;
+  flex-direction: column;
+  gap: 1.2rem;
+  margin-top: 1rem;
+
+  button {
+    padding: 0.65rem 1rem;
+    border-radius: 1.25rem;
+    border: none;
+    background-color: lighten($bg-color-secondary, 1%);
+    font-size: 1rem;
+    color: $txt-color-primary;
+    cursor: pointer;
+
+
+  }
+
+  button:hover {
+    background-color: lighten($bg-color-secondary, 4%);
+  }
+}
+</style>
--- src/components/reusable/RouteDefaultNavbar.vue
+++ src/components/reusable/RouteDefaultNavbar.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <nav :class="$style.settingsNavbar">
-      <router-link to="/"><img src="@/assets/icons/navigation/arrow_back.svg" alt="" /></router-link>
+      <img src="@/assets/icons/navigation/arrow_back.svg" alt="" @click="goBackOnePage" />
       <h2>{{ currentRouteName }}</h2>
     </nav>
   </div>
@@ -9,14 +9,19 @@
 
 <script setup lang="ts">
 import { computed } from 'vue';
-import { useRoute } from 'vue-router';
+import { useRoute, useRouter } from 'vue-router';
 
 const route = useRoute();
+const router = useRouter();
 
 const currentRouteName = computed(() => {
-	let routeName = route.name as string;
-	return routeName ? routeName.charAt(0).toUpperCase() + routeName.slice(1) : '';
-}); 
+  let routeName = route.name as string;
+  return routeName ? routeName.charAt(0).toUpperCase() + routeName.slice(1) : '';
+});
+
+const goBackOnePage = () => {
+  router.go(-1)
+}
 </script>
 
 <style module lang="scss">
