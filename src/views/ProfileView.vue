3d825daa87ffb362400330f7ff6f9c6e52cd7f2b
ReQ1964 <slomianykacper4@gmail.com>
2023-08-30 20:16:27 +0200

    feat: add ProfileOptions and icons to it, incorporate in ProfileView

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -1,11 +1,13 @@
 <template>
   <main>
     <ProfilePhoto />
+    <ProfileOptions />
   </main>
 </template>
 
 <script setup lang="ts">
 import ProfilePhoto from '@/components/profile/ProfilePhoto.vue';
+import ProfileOptions from '@/components/profile/ProfileOptions.vue';
 </script>
 
 <style lang="scss"></style>
