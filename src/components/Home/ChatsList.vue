bcfcc7da1649f7a55053ec30d40af1dc4e023aac
ReQ1964 <slomianykacper4@gmail.com>
2023-08-30 18:08:36 +0200

    fix: update Chats interface, remove console.log

--- src/components/Home/ChatsList.vue
+++ src/components/Home/ChatsList.vue
@@ -1,10 +1,10 @@
 <template>
-  <HalfCircleSpinner v-if="state.loading && !state.error" color=#dfeed8 />
-  <ErrorMessage v-if="state.error" :message="state.errorMessage" />
-  <NoChats v-if="state.chats.length === 0 && !state.loading" />
-  <ul v-else :class="$style.chatsList">
-    <UserComponent v-for="chat in state.chats" :key="chat.id" :chat="chat" />
-  </ul>
+	<HalfCircleSpinner v-if="state.loading && !state.error" color=#dfeed8 />
+	<ErrorMessage v-if="state.error" :message="state.errorMessage" />
+	<NoChats v-if="state.chats.length === 0 && !state.loading" />
+	<ul v-else :class="$style.chatsList">
+		<UserComponent v-for="chat in state.chats" :key="chat.id" :chat="chat" />
+	</ul>
 </template>
 <script setup lang="ts">
 import { reactive, onMounted } from 'vue';
@@ -38,7 +38,6 @@ const getChatrooms = async () => {
 			const friend = members.find(({ username }) => username !== loggedInUserUsername);
 			return { id: _id, friend, last_message };
 		});
-		console.log(state.chats);
 
 	} catch (error: unknown) {
 		if (error instanceof Error) {
@@ -56,16 +55,16 @@ onMounted(getChatrooms);
 </script>
 <style module lang="scss">
 .chatsList {
-  display: flex;
-  flex-direction: column;
-  padding: 1rem;
-  gap: 2.5rem;
-  margin-top: 1rem;
+	display: flex;
+	flex-direction: column;
+	padding: 1rem;
+	gap: 2.5rem;
+	margin-top: 1rem;
 }
 
 .noChats {
-  color: $txt-color-primary;
-  text-align: center;
-  margin-top: 3rem;
+	color: $txt-color-primary;
+	text-align: center;
+	margin-top: 3rem;
 }
 </style>
--- src/layouts/DefaultLayout.vue
+++ src/layouts/DefaultLayout.vue
@@ -8,4 +8,3 @@ import RouteDefaultNavbar from '@/components/reusable/RouteDefaultNavbar.vue';
 </script>
 
 <style module lang="scss"></style>
-<!-- I changed that the SettingsLayout is now a DefaultLayout with DefaultRouteNavbar and added ProfileView to routing -->
