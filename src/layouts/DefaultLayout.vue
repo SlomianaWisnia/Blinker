ee0f2c4a0746d554ad43559dabfbce2494e5aec4
ReQ1964 <slomianykacper4@gmail.com>
2023-08-30 18:03:08 +0200

    refactor: change SettingsLayout to DefaultLayout and SettingsNav to RouteDefaultNav

---
+++ src/layouts/DefaultLayout.vue
@@ -0,0 +1,11 @@
+<template>
+  <RouteDefaultNavbar />
+  <router-view />
+</template>
+
+<script setup lang="ts">
+import RouteDefaultNavbar from '@/components/reusable/RouteDefaultNavbar.vue';
+</script>
+
+<style module lang="scss"></style>
+<!-- I changed that the SettingsLayout is now a DefaultLayout with DefaultRouteNavbar and added ProfileView to routing -->
