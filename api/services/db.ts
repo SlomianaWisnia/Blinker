afb26b2af1bff10ab5064b5c99ad2691eaaf7242
mtw-programmer <visnia58@gmail.com>
2023-08-31 14:42:03 +0200

    feat: add mongoose on event listeners for db service
    add log and process.exit if needed for open, disconnected, reconnected, connected and SIGNIT listeners

--- api/services/db.ts
+++ api/services/db.ts
@@ -4,6 +4,32 @@ import log from '../utils/log';
 
 dotenv.config({ path: `config/${process.env.NODE_ENV}.env` });
 
-export default mongoose.connect(`mongodb://${process.env.DB_HOST}/${process.env.DB_NAME}`)
-  .then(() => log.init({ label: 'DB', message: 'Connected to MongoDB' }))
-  .catch((err) => log.error({ label: 'DB', message: err }));
+const config = process.env;
+
+mongoose.connect(`mongodb://${config.DB_HOST}:${config.DB_PORT}/${config.DB_NAME}`)
+  .catch((ex) => {
+    log.error({ label: 'DB', message: ex });
+    process.exit(1);
+  });
+
+mongoose.connection.on('open', () =>
+  log.info({ label: 'DB', message: `Opened connection to ${config.DB_NAME} database on port ${config.DB_PORT}` })
+);
+
+mongoose.connection.on('disconnected', () => {
+  log.error({ label: 'DB', message:  `Lost connection to MongoDB database!` });
+});
+
+mongoose.connection.on('reconnected', () => {
+  log.info({ label: 'DB', message:  `Reconnected to MongoDB database` });
+});
+
+mongoose.connection.on('connected', () =>
+  log.info({ label: 'DB', message: 'Established MongoDB connection' })
+);
+
+process.on('SIGINT', () => {
+  log.error({ label: 'DB', message: 'Forced to close MongoDB connection!' });
+  mongoose.connection.close();
+  process.exit(1);
+});
