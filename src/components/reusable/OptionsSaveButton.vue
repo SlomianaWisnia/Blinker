3296cd339b6e2d566941703f74a5eb20f35aaba5
ReQ1964 <slomianykacper4@gmail.com>
2023-08-31 14:39:57 +0200

    feat: add two icons to ProfilePhotoEdit and OptionsSaveButton with prepared async logic

---
+++ src/components/reusable/OptionsSaveButton.vue
@@ -0,0 +1,83 @@
+<template>
+	<ErrorMessage v-if="state.error" :message="state.errorMessage" />
+	<button @click="optionsSaveHandler" :class="$style.saveBtn" :disabled="state.loading">
+		<HalfCircleSpinner v-if="state.loading && !state.error" color=#dfeed8 :size=30 :class="$style.optSaveBtn" />
+		<p v-if="!state.loading">Save</p>
+	</button>
+</template>
+
+<script setup lang="ts">
+import { reactive } from 'vue';
+import axios from 'axios';
+import { HalfCircleSpinner } from 'epic-spinners';
+import ErrorMessage from '@/components/reusable/ErrorMessage.vue';
+
+const props = defineProps({
+	payload: {
+		type: Object,
+		default: undefined,
+	},
+	endpoint: {
+		type: String,
+		default: undefined
+	}
+});
+
+const state = reactive({
+	loading: false,
+	error: false,
+	errorMessage: '',
+});
+
+const optionsSaveHandler = async () => {
+	state.loading = true;
+
+	try {
+		await axios.put(`${props.endpoint}`, {
+			...props.payload
+		});
+	} catch (error: unknown) {
+		if (error instanceof Error) {
+			state.error = true;
+			state.errorMessage = error.message;
+			console.error(error.message);
+		}
+	} finally {
+		state.loading = false;
+	}
+};
+
+</script>
+
+<style module lang="scss">
+.saveBtn {
+	position: absolute;
+	bottom: 18px;
+	right: 18px;
+	font-size: 0.9rem;
+	color: $txt-color-primary;
+	border-radius: 1.25rem;
+	border: none;
+	height: 40px;
+	width: 80px;
+	background-color: lighten($bg-color-secondary, 1%);
+	cursor: pointer;
+
+}
+
+.optSaveBtn {
+	position: absolute !important;
+	top: 50%;
+	left: 50%;
+	transform: translate(-50%, -50%);
+}
+
+.saveBtn:hover {
+	background-color: lighten($bg-color-secondary, 4%);
+}
+
+.saveBtn:disabled {
+	opacity: 0.6;
+	cursor: not-allowed;
+}
+</style>
