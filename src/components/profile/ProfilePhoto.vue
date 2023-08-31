2434f864501e8dc4597d9d1ba0f831d62dd9cec8
ReQ1964 <slomianykacper4@gmail.com>
2023-08-31 12:54:40 +0200

    feat: add photo-edit route, close icon, optional 'close' meta tag for Navigation Bar

--- src/components/profile/ProfilePhoto.vue
+++ src/components/profile/ProfilePhoto.vue
@@ -1,35 +1,42 @@
 <template>
-  <div :class="$style.profilePhoto">
-    <UserAvatar :size="110" />
-    <button>Edit photo</button>
-  </div>
+	<div :class="$style.profilePhoto">
+		<UserAvatar :size="110" />
+		<button @click="goToPhotoEdit">Edit photo</button>
+	</div>
 </template>
 
 <script setup lang="ts">
+import { useRouter } from 'vue-router';
 import UserAvatar from '@/components/reusable/UserAvatar.vue';
+
+const router = useRouter();
+
+const goToPhotoEdit = () => {
+	router.push('/profile/photo-edit');
+};
 </script>
 
 <style module lang="scss">
 .profilePhoto {
-  @include flex-center;
-  flex-direction: column;
-  gap: 1.2rem;
-  margin-top: 1rem;
+	@include flex-center;
+	flex-direction: column;
+	gap: 1.2rem;
+	margin-top: 1rem;
 
-  button {
-    padding: 0.65rem 1rem;
-    border-radius: 1.25rem;
-    border: none;
-    background-color: lighten($bg-color-secondary, 1%);
-    font-size: 1rem;
-    color: $txt-color-primary;
-    cursor: pointer;
+	button {
+		padding: 0.65rem 1rem;
+		border-radius: 1.25rem;
+		border: none;
+		background-color: lighten($bg-color-secondary, 1%);
+		font-size: 1rem;
+		color: $txt-color-primary;
+		cursor: pointer;
 
 
-  }
+	}
 
-  button:hover {
-    background-color: lighten($bg-color-secondary, 4%);
-  }
+	button:hover {
+		background-color: lighten($bg-color-secondary, 4%);
+	}
 }
 </style>
