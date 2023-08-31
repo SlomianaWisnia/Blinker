6651d52bb770fa1b961b370ba19bf5bad5b8f0d9
ReQ1964 <slomianykacper4@gmail.com>
2023-08-31 14:38:31 +0200

    fix: errorMessage bad styling, string to num in FormButton

--- src/components/reusable/ErrorMessage.vue
+++ src/components/reusable/ErrorMessage.vue
@@ -1,5 +1,5 @@
 <template>
-  <p :class="$style.errorMsg">{{ props.message }}</p>
+	<p :class="$style.errorMsg">{{ props.message }}</p>
 </template>
 <script setup lang="ts">
 const props = defineProps({
@@ -11,11 +11,12 @@ const props = defineProps({
 </script>
 <style module lang="scss">
 .errorMsg {
-  position: fixed;
-  top: 40%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  width: 80%;
-  color: #c54b12;
+	position: fixed;
+	top: 60%;
+	text-align: center;
+	left: 50%;
+	transform: translate(-50%, -50%);
+	width: 80%;
+	color: #c54b12;
 }
 </style>
