f5699af70d47e37ab9056d7de86b165e72826b72
ReQ1964 <slomianykacper4@gmail.com>
2023-08-31 14:39:57 +0200

    feat: add two icons to ProfilePhotoEdit and OptionsSaveButton with prepared async logic

--- src/components/profile/ProfilePhotoEdit.vue
+++ src/components/profile/ProfilePhotoEdit.vue
@@ -1,11 +1,25 @@
 <template>
 	<div :class="$style.profilePhotoEdit">
-		<UserAvatar :size="110" />
+		<UserAvatar :size="140" />
+		<div :class="$style.options">
+			<div>
+				<img :src="cameraIcon" alt="camera icon">
+				<p>Camera</p>
+			</div>
+			<div>
+				<img :src="photoIcon" alt="photo icon">
+				<p>Photo</p>
+			</div>
+		</div>
+		<OptionsSaveButton />
 	</div>
 </template>
 
 <script setup lang="ts">
 import UserAvatar from '@/components/reusable/UserAvatar.vue';
+import cameraIcon from '@/assets/icons/profile/camera.svg';
+import photoIcon from '@/assets/icons/profile/photo.svg';
+import OptionsSaveButton from '@/components/reusable/OptionsSaveButton.vue';
 </script>
 
 <style module lang="scss">
@@ -15,5 +29,26 @@ import UserAvatar from '@/components/reusable/UserAvatar.vue';
 	gap: 1.2rem;
 	margin-top: 1rem;
 
+	.options {
+		@include flex-center;
+		gap: 2rem;
+
+		img {
+			width: 58px;
+			border-radius: 1.25rem;
+			padding: 0.7rem;
+			background-color: lighten($bg-color-secondary, 1%);
+			cursor: pointer;
+		}
+
+		img:hover {
+			background-color: lighten($bg-color-secondary, 4%);
+		}
+
+		p {
+			text-align: center;
+			margin-top: 0.4rem;
+		}
+	}
 }
 </style>
