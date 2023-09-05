74d10994f063ca07750d4fb27e676622fbf7ecd2
mtw-programmer <visnia58@gmail.com>
2023-09-05 21:58:59 +0200

    feat: add updateAvatar route to the app

--- api/index.ts
+++ api/index.ts
@@ -20,6 +20,7 @@ import getChatMessages from './routes/getChatMessages';
 import sendMessage from './routes/sendMessage';
 import authVerify from './routes/auth-verify';
 import logOut from './routes/logOut';
+import updateAvatar from './routes/updateAvatar';
 import log from './utils/log';
 
 import ChatRoom from './models/ChatRoom';
@@ -85,6 +86,7 @@ app.use('/api/get-last-messages', [authorization, getLastMessages]);
 app.use('/api/messages', [authorization, getChatMessages]);
 app.use('/api/send-message', [authorization, sendMessage]);
 app.use('/api/auth-verify', [authorization, authVerify]);
+app.use('/api/update-avatar', [authorization, updateAvatar]);
 app.use('/api/logout', logOut);
 
 io.on('connection', async (socket:Socket & { request: { session: { userId: string } } }) => {
