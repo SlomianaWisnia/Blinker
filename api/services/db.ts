1492a0f4a52625c018fc63648d4ef01f12c88a22
mtw-programmer <visnia58@gmail.com>
2023-09-12 14:11:02 +0200

    refactor: use exit util on mongoose SIGNIT

--- api/services/db.ts
+++ api/services/db.ts
@@ -2,6 +2,7 @@ import mongoose from 'mongoose';
 import log from '../utils/log';
 
 import config from '../utils/config';
+import exit from '../utils/errorHandling/exit';
 
 mongoose.connect(`mongodb://${config.DB_HOST}:${config.DB_PORT}/${config.DB_NAME}`)
   .catch((ex) => {
@@ -26,7 +27,6 @@ mongoose.connection.on('connected', () =>
 );
 
 process.on('SIGINT', () => {
-  log.error({ label: 'DB', message: 'Forced to close MongoDB connection!' });
   mongoose.connection.close();
-  process.exit(1);
+  exit({ label: 'DB', message: 'Forced to close MongoDB connection!' });
 });
