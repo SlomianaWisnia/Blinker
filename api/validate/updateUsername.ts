d50d1e0f5716869c2b779877140b381505898a64
mtw-programmer <visnia58@gmail.com>
2023-09-19 17:58:10 +0200

    fix: fix updateUsername validator

--- api/validate/updateUsername.ts
+++ api/validate/updateUsername.ts
@@ -1,7 +1,7 @@
 import Joi from 'joi';
 
 const schema = Joi.object().keys({
-  username: Joi.string().min(5).max(70).required(),
+  username: Joi.string().min(5).max(50).required(),
 }).unknown(true);
 
 export default (user:{ username: string }) => schema.validate(user);
